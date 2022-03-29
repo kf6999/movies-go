db610ef3c45509a5cb87180bf305ff150e8337e9
Kenneth Fan <kf6999@rit.edu>
2022-03-29 11:48:57 -0400

    json formatting

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -5,13 +5,13 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	data := map[string]string{
-		"status":      "ok",
-		"environment": app.config.env,
-		"version":     version,
+	env := envelope{
+		"status": "available", "system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version},
 	}
 
-	err := app.writeJSON(w, http.StatusOK, data, nil)
+	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
